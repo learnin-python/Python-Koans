d49d0ceed34fa11c9589f13ee8fa08514bc00108
sharkzuria <baumabulondo@gmail.com>
2020-10-26 22:58:23 +0300

    Decorating with classes

--- koans/about_decorating_with_functions.py
+++ koans/about_decorating_with_functions.py
@@ -29,4 +29,4 @@ class AboutDecoratingWithFunctions(Koan):
         return name
 
     def test_decorators_can_change_a_function_output(self):
-        self.assertEqual("<llama>", self.render_tag('llama'))
+        self.assertEqual("<llama/>", self.render_tag('llama'))
