5206a8b10033ba82b52c7bd69ed1b5f6b44565c8
sharkzuria <baumabulondo@gmail.com>
2020-11-02 23:25:24 +0300

    Importing a module in subfolder using an absolute path

--- koans/about_packages.py
+++ koans/about_packages.py
@@ -57,4 +57,4 @@ class AboutPackages(Koan):
         # Import contemplate_koans.py/koans/a_package_folder/a_module.py
         from koans.a_package_folder.a_module import Duck
 
-        self.assertEqual(__, Duck.__module__)
+        self.assertEqual('koans.a_package_folder.a_module', Duck.__module__)
