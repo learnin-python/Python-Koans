0653f385a4144a1be270ca0d71679e624cb28a6f
sharkzuria <baumabulondo@gmail.com>
2020-11-12 23:10:02 +0300

    Nearly Enlightened

--- koans/about_deleting_objects.py
+++ koans/about_deleting_objects.py
@@ -78,7 +78,7 @@ class AboutDeletingObjects(Koan):
         self.assertEqual('Senor Ninguno', cowboy.name)
 
         del cowboy.name
-        self.assertEqual(__, cowboy.name)
+        self.assertEqual('The man win no name', cowboy.name)
 
 
     # ====================================================================
